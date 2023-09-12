6fe9b13ff88d198111993e2e2f5414f843586ef4
Alexcarp-ar <alexriverplate06@gmail.com>
2023-09-12 17:18:54 -0300

    cambio

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,10 @@ function App() {
 
   return (
     <>
-      <h1>Esto es una app</h1>      
+      {/* Cambios de alexcarp */}
+      <h2>Estos son cambios de alexcarp</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique, qui praesentium, voluptas id quae iusto voluptates repellat doloremque obcaecati fugit minus itaque quasi necessitatibus deleniti amet nostrum. Quod, unde expedita!
+      In sunt delectus atque enim veritatis dolorum laudantium aliquam eos temporibus dolorem recusandae perferendis accusamus repellendus at pariatur accusantium nisi tenetur quisquam, aspernatur praesentium obcaecati iste ad cupiditate? Ullam, numquam!</p>     
     </>
   )
 }
